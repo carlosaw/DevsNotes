71f5d8eb89c88c8916458d13661d7fd68605b0a7
unknown <carlosfreevg@gmail.com>
2021-01-27 21:43:32 -0400

    Estrutura

---
+++ src/pages/ListScreen/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { useNavigation } from '@react-navigation/native';
+import {
+    Container,
+    Texto,
+    Botao
+} from './styles';
+
+export default () => {
+    const navigation = useNavigation();
+    return (
+        <Container>
+            <Texto>TELA DE LISTAR</Texto>
+            <Botao title="Ir para EDITAR" onPress={()=>navigation.navigate('EditNote')} />
+        </Container>
+    );
+}
